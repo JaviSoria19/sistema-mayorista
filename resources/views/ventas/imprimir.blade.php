bc8d9f9170f5d07ffa2db9c71266cbee8601bf9c
JaviSoria19 <javisoar.2002@gmail.com>
2025-10-02 14:13:42 -0400

    Se cambió la vista de la venta a petición del cliente.

--- resources/views/ventas/imprimir.blade.php
+++ resources/views/ventas/imprimir.blade.php
@@ -5,7 +5,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>{{ helper_tituloPagina() }} | VENTA N° {{ $venta->idVenta }} {{ $venta->estado == '0' ? '(ELIMINADA)' : '' }}</title>
+        <title>{{ helper_tituloPagina() }} | VENTA N° {{ $venta->idVenta }}
+            {{ $venta->estado == '0' ? '(ELIMINADA)' : '' }}</title>
         <!-- Bootstrap CSS -->
         <link href="{{ asset('/public/dependencies/bootstrapdompdf.css') }}" rel="stylesheet">
     </head>
@@ -28,93 +29,135 @@
 
             .watermark {
                 position: fixed;
-                top: 34.5%;
+                top: 0%;
                 left: 28%;
                 width: 300px;
                 opacity: 0.15;
                 z-index: -1000;
             }
+
+            .table-container {
+                display: flex;
+                flex-direction: column;
+            }
+
+            .table-container table {
+                margin: 0;
+            }
+
+            .table-container table td {
+                padding: 0;
+                padding-left: 2px;
+                padding-right: 2px;
+            }
+
+            .table-container table th {
+                padding: 0;
+                padding-left: 2px;
+                padding-right: 2px;
+            }
+
+            .inicio {
+                margin: 0;
+                padding: 0;
+            }
         </style>
         <img src="{{ public_path('img/logo_sistema_mayorista.jpg') }}" class="watermark">
 
-        <p class="titulo bg-{{ $venta->estado == '0' ? 'danger' : 'info' }} text-white p-1 m-2 text-center rounded align-middle">VENTA N° {{ $venta?->idVenta }} {{ $venta->estado == '0' ? '(ELIMINADA)' : '' }}
+        <p
+            class="font-weight-bold bg-{{ $venta->estado == '0' ? 'danger' : 'info' }} text-white m-2 text-center rounded align-middle">
+            VENTA N° {{ $venta?->idVenta }}
+            {{ $venta->estado == '0' ? '(ELIMINADA EL ' . date('d/m/Y H:i:s', strtotime($venta?->fechaEliminacion)) . ')' : '' }}
         </p>
 
-        <div class="border border-info rounded p-2 m-2">
+        <div class="table-container border border-info rounded p-2 m-2">
             <table class="table table-bordered">
                 <tr>
                     <td class="font-weight-bold">
                         <span class="text-info">Empleado:</span> {{ $venta?->empleado->nombreEmpleado }}<br>
                         <span class="text-info">Usuario:</span> {{ $venta?->usuario->nombreUsuario }}
                     </td>
-                    <td class="font-weight-bold">
-                        <span class="text-info">Fecha de Registro:</span>
+                    <td class="font-weight-bold text-right">
+                        <span class="text-info">F. Registro:</span>
                         {{ date('d/m/Y H:i:s', strtotime($venta?->fechaRegistro)) }}<br>
-                        <span class="text-info">Fecha de Actualización:</span>
+                        <span class="text-info">F. Actualización:</span>
                         {{ date('d/m/Y H:i:s', strtotime($venta?->fechaActualizacion)) }}
                     </td>
                 </tr>
                 @if ($venta->fechaEliminacion)
-                <tr>
-                    <td class="font-weight-bold text-center" colspan="2">
-                        <span class="text-info">Fecha de Eliminación:</span>
-                        {{ date('d/m/Y H:i:s', strtotime($venta?->fechaEliminacion)) }}
-                    </td>
-                </tr>
+                    <tr>
+                        <td class="font-weight-bold text-center" colspan="2">
+                            <span class="text-info">Fecha de Eliminación:</span>
+                            {{ date('d/m/Y H:i:s', strtotime($venta?->fechaEliminacion)) }}
+                        </td>
+                    </tr>
                 @endif
             </table>
 
-            <table class="table table-bordered">
-                <tr>
-                    <td class="font-weight-bold">
-                        <span class="text-info">Cliente:</span> {{ $venta?->cliente->nombreCliente }} - <span
-                            class="text-info">Celular:</span> {{ $venta?->cliente->celular }}<br>
-                        <span class="text-info">Procedencia:</span> {{ $venta?->cliente->procedencia }}
-                    </td>
-                </tr>
-            </table>
-        </div>
+            <div class="text-center font-weight-bold"><span class="text-info">Cliente:</span>
+                {{ $venta?->cliente->nombreCliente }} - <span class="text-info">Celular:</span>
+                {{ $venta?->cliente->celular }} - <span class="text-info">Procedencia:</span>
+                {{ $venta?->cliente->procedencia }}</div>
 
-        <p class="subtitulo text-info text-center">DETALLES</p>
+            @php
+                $productosAgrupados = collect($venta?->productos)->groupBy(function ($producto) {
+                    // Clave de agrupación (marca + nombre + precio)
+                    return $producto->marca->nombreMarca .
+                        '|' .
+                        $producto->nombreProducto .
+                        '|' .
+                        $producto->pivot->precioUSD;
+                });
 
-        <div class="m-2">
-            <table class="table table-bordered table-striped">
+                $total = 0;
+            @endphp
+
+            <table class="table table-bordered table-striped" style="border-color:1px black">
                 <thead class="bg-secondary text-light text-center">
                     <tr>
                         <th>#</th>
                         <th>PRODUCTO</th>
-                        <th>PRECIO (USD)</th>
+                        <th>CANTIDAD</th>
+                        <th>PRECIO UNIT. (USD)</th>
+                        <th>SUBTOTAL (USD)</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @php $total = 0; @endphp
-                    @foreach ($venta?->productos as $index => $producto)
+                    @foreach ($productosAgrupados as $index => $grupo)
                         @php
-                            $total += $producto->pivot->precioUSD;
+                            $cantidad = $grupo->count();
+                            $precioUnitario = $grupo->first()->pivot->precioUSD;
+                            $subtotal = $cantidad * $precioUnitario;
+                            $total += $subtotal;
+                            $producto = $grupo->first();
                         @endphp
-
                         <tr>
-                            <td class="text-center"><b>{{ $index + 1 }}.</b></td>
-                            <td><span class="text-info">{{ $producto->codigoProducto }}</span>
-                                {{ $producto->marca->nombreMarca }} {{ $producto->nombreProducto }}</td>
-                            <td class="text-right">{{ number_format($producto->pivot->precioUSD, 2) }}</td>
+                            <td class="text-center"><b>{{ $loop->iteration }}.</b></td>
+                            <td>
+                                {{-- <span class="text-info">{{ $producto->codigoProducto }}</span> --}}
+                                {{ $producto->marca->nombreMarca }} {{ $producto->nombreProducto }}
+                            </td>
+                            <td class="text-center">{{ $cantidad }}</td>
+                            <td class="text-right">{{ number_format($precioUnitario, 2) }}</td>
+                            <td class="text-right">{{ number_format($subtotal, 2) }}</td>
                         </tr>
                     @endforeach
-                </tbody>
-                <tfoot>
                     <tr>
-                        <th colspan="2" class="text-right">TOTAL (USD):</th>
-                        <th class="text-right">{{ number_format($total, 2) }}</th>
+                        <td colspan="4" class="text-right">TOTAL (USD):</td>
+                        <td class="text-right font-weight-bold">{{ number_format($total, 2) }}</td>
                     </tr>
-                </tfoot>
+                </tbody>
             </table>
+
+
+            <p class="subtitulo text-info text-center m-0">--- PAGOS ---</p>
 
             <table class="table table-bordered table-striped">
                 <thead class="bg-secondary text-light text-center">
                     <tr>
                         <th>#</th>
                         <th>FECHA</th>
-                        <th>PAGOS (USD)</th>
+                        <th>PAGO (USD)</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -122,7 +165,8 @@
                         <tr>
                             <td class="text-center"><b>{{ $index + 1 }}.</b></td>
                             <td>{{ date('d/m/Y H:i:s', strtotime($pago->fechaRegistro)) }}</td>
-                            <td class="text-right"><span class="text-success font-weight-bold">{{ $pago->pagoUSD }}</span></td>
+                            <td class="text-right"><span
+                                    class="text-success font-weight-bold">{{ $pago->pagoUSD }}</span></td>
                         </tr>
                     @endforeach
                 </tbody>
@@ -133,8 +177,16 @@
                     </tr>
                 </tfoot>
             </table>
+        </div>
 
-            <p class="text-center">Muchas gracias por tu compra!</p>
+        {{-- 
+
+        <div class="m-2">
+            
+
+             --}}
+
+        <p class="text-center">Muchas gracias por tu compra!</p>
         </div>
 
 
